beadd2478e46d0132e6b0eb72dac5595b28bf0b7
홍준성 <ghdwnstjd13@gmail.com>
2022-10-06 09:54:14 +0900

    New Project > git init

---
+++ src/main/java/spring/study/batch/SpringStudyBatchApplication.java
@@ -0,0 +1,13 @@
+package spring.study.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyBatchApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyBatchApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/spring/study/batch/SpringStudyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.study.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyBatchApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
