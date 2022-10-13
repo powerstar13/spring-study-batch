9489969a8e1b969f0e93ee4d4afa808b584de157
홍준성 <ghdwnstjd13@gmail.com>
2022-10-13 11:17:18 +0900

    Partition Step 적용

--- src/main/java/spring/study/batch/part4/UserRepository.java
+++ src/main/java/spring/study/batch/part4/UserRepository.java
@@ -1,10 +1,18 @@
 package spring.study.batch.part4;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    
     List<User> findAllByUpdatedDate(LocalDate updatedDate);
+    
+    @Query(value = "SELECT MIN(u.id) FROM users u")
+    long findMinId();
+    
+    @Query(value = "SELECT MAX(u.id) FROM users u")
+    long findMaxId();
 }
